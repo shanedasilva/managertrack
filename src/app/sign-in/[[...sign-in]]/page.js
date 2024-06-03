d5209cad4fd7a16b25af5d45007cfb46cbaf0533
Shane Da Silva <shanemichaeldasilva@gmail.com>
2024-06-03 10:58:02 -0700

    add sign-in sign-up background image

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,23 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div class="relative flex min-h-screen flex-col items-center justify-center pb-4 pt-10 font-sans md:pb-0 md:pt-0">
+      <div className="fixed inset-0 z-0">
+        <img
+          alt="sign-in"
+          loading="lazy"
+          decoding="async"
+          class="pointer-events-none absolute inset-0 object-cover object-center absolute w-full h-full"
+          src="https://dashboard.clerk.com/assets/signin-bg.svg"
+          style={{
+            inset: " 0px",
+            color: "transparent",
+          }}
+        />
+      </div>
+
+      <SignIn />
+    </div>
+  );
 }
--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -1,5 +1,26 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="relative flex min-h-screen flex-col items-center justify-center pb-4 pt-10 font-sans md:pb-0 md:pt-0">
+      <div className="fixed inset-0 z-0">
+        <img
+          alt="sign-in"
+          loading="lazy"
+          decoding="async"
+          class="pointer-events-none absolute inset-0 object-cover object-center absolute w-full h-full"
+          src="https://dashboard.clerk.com/assets/signin-bg.svg"
+          style={{
+            position: "absolute",
+            height: "100%",
+            width: "100%",
+            inset: " 0px",
+            color: "transparent",
+          }}
+        />
+      </div>
+
+      <SignUp />
+    </div>
+  );
 }
