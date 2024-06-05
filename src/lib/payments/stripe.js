16d36305cc7a17ca3847d9ce7ee8574ec91ecf52
Shane Da Silva <shanemichaeldasilva@gmail.com>
2024-06-05 12:55:24 -0700

    fix typo

--- src/lib/payments/stripe.js
+++ src/lib/payments/stripe.js
@@ -71,8 +71,8 @@ export async function createStripeCheckoutSession(customerId) {
         },
       ],
       mode: "payment",
-      success_url: `${NEXT_PUBLIC_BASE_APP_URL}/api/stripe/checkout-success?stripe_session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${NEXT_PUBLIC_BASE_APP_URL}/cancel`,
+      success_url: `${process.env.NEXT_PUBLIC_BASE_APP_URL}/api/stripe/checkout-success?stripe_session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.NEXT_PUBLIC_BASE_APP_URL}/cancel`,
     };
 
     return stripe.checkout.sessions.create(checkoutParams);
