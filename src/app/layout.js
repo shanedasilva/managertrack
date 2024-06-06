2f4bec6a01398e3f6f17219d3a23f6b28a3c4baf
Shane Da Silva <shanemichaeldasilva@gmail.com>
2024-06-06 10:36:42 -0700

    Add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter as FontSans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
+
 import "../styles/globals.css";
 
 import { cn } from "@/lib/utils";
@@ -33,6 +35,7 @@ export default function RootLayout({ children }) {
           )}
         >
           <main>{children}</main>
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
