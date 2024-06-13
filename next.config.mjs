25be7f21d095284918268cc74b80da0bb6f9394d
Shane Da Silva <shanemichaeldasilva@gmail.com>
2024-06-13 15:25:53 -0700

    disable @next/next/no-img-element for now

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "tailwindui.com",
       },
+      {
+        protocol: "https",
+        hostname: "wellfound.com",
+      },
     ],
   },
 };
